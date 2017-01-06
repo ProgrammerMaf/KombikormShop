a87186836acf0ace33cde49e12daf4a4293c29b6
ProgrammerMaf <VisualMaf@yandex.ru>
2017-01-06 19:35:42 +0500

    Favicon added

--- ishop/ishop/urls.py
+++ ishop/ishop/urls.py
@@ -17,8 +17,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'', include('AnimalFeed.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/media/images/favicon.ico')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
